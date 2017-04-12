170a725ac16f339be50d7bbc28a084b3bb84c0d5
YowChinHuang <s6450319@gmail.com>
2017-04-13 02:13:21 +0800

    Add files via upload

---
+++ OpenBor_HealthChanger/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OpenBor_HealthChanger {
+    partial class frmMain {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該公開 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblPanel = new System.Windows.Forms.Label();
+            this.lstFile = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblPanel
+            // 
+            this.lblPanel.AllowDrop = true;
+            this.lblPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPanel.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblPanel.Location = new System.Drawing.Point(0, 0);
+            this.lblPanel.Name = "lblPanel";
+            this.lblPanel.Size = new System.Drawing.Size(389, 273);
+            this.lblPanel.TabIndex = 0;
+            this.lblPanel.Text = "Drop OpenBor Game Files or Dirs";
+            this.lblPanel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblPanel_DragDrop);
+            this.lblPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblPanel_DragEnter);
+            // 
+            // lstFile
+            // 
+            this.lstFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFile.FormattingEnabled = true;
+            this.lstFile.ItemHeight = 12;
+            this.lstFile.Location = new System.Drawing.Point(0, 0);
+            this.lstFile.Name = "lstFile";
+            this.lstFile.Size = new System.Drawing.Size(389, 268);
+            this.lstFile.TabIndex = 1;
+            this.lstFile.DoubleClick += new System.EventHandler(this.lstFile_DoubleClick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 273);
+            this.Controls.Add(this.lblPanel);
+            this.Controls.Add(this.lstFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenBor Enemy Health Changer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPanel;
+        private System.Windows.Forms.ListBox lstFile;
+    }
+}
+
---
+++ OpenBor_HealthChanger/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpenBor_HealthChanger {
+    public partial class frmMain : Form {
+
+        private Boolean ProcessFinish;
+
+        public frmMain() {
+            InitializeComponent();
+            ProcessFinish = false;
+        }
+
+        private void lblPanel_DragEnter(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void lblPanel_DragDrop(object sender, DragEventArgs e) {
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            lblPanel.Visible = false;
+            ProcessFinish = false;
+            lstFile.Items.Clear();
+
+            foreach (string file in files) {
+
+                if ((File.GetAttributes(file) & FileAttributes.Directory) == FileAttributes.Directory) {
+                    DirectoryInfo DI = new DirectoryInfo(file);
+
+                    foreach (FileInfo fileindirs in DI.GetFiles("*.txt", SearchOption.AllDirectories)) {
+                        if (ProcessFile(fileindirs.FullName) == true) {
+                            lstFile.Items.Add(fileindirs.FullName);
+                            lstFile.SelectedIndex = lstFile.Items.Count - 1;
+                        }
+                        Application.DoEvents();
+                    }
+                    DI = null;
+                } else {
+
+                    if (Path.GetExtension(file).ToLower().Equals(".txt") == true) {
+                        if (ProcessFile(file) == true) {
+                            lstFile.Items.Add(file);
+                            lstFile.SelectedIndex = lstFile.Items.Count - 1;
+                        }
+                        Application.DoEvents();
+                    }
+                }
+            }
+            lstFile.Items.Add("========================================");
+            lstFile.Items.Add("Double Click to Finish!");
+            lstFile.SelectedIndex = lstFile.Items.Count - 1;
+            ProcessFinish = true;
+        }
+
+        private void lstFile_DoubleClick(object sender, EventArgs e) {
+
+            if (ProcessFinish == true) {
+                lblPanel.Visible = true;
+            }
+        }
+
+        Boolean ProcessFile(String filename) {
+            string[] lines = File.ReadAllLines(filename);
+            Boolean DataChanged = false;
+
+            if (filename.IndexOf("\\chars\\", StringComparison.CurrentCultureIgnoreCase) != -1) {
+                Boolean KeyWord = false;
+
+                for (int i = 0;i < lines.Length;i++) {
+
+                    if ((lines[i].IndexOf("type", StringComparison.CurrentCultureIgnoreCase) != -1) && (lines[i].IndexOf("enemy", StringComparison.CurrentCultureIgnoreCase) != -1)) {
+                        KeyWord = true;
+                    }
+                    if (lines[i].IndexOf("health", StringComparison.CurrentCultureIgnoreCase) != -1) {
+                        string OldVal = lines[i].ToLower().Replace("health", "").Trim();
+
+                        lines[i] = lines[i].Replace(OldVal, "1");
+                        DataChanged = true;
+                    }
+                }
+                if (KeyWord == false) {
+                    DataChanged = false;
+                }
+
+            } else {
+
+                for (int i = 0; i < lines.Length;i++) {
+                    if ((lines[i].IndexOf("health", StringComparison.CurrentCultureIgnoreCase) != -1) && (lines[i].StartsWith("#") == false)) {
+                        string OldVal = lines[i].ToLower().Replace("health", "").Trim();
+
+                        lines[i] = lines[i].Replace(OldVal, "1");
+                        DataChanged = true;
+                    }
+                }
+            }
+            if (DataChanged == true) {
+                File.WriteAllLines(filename, lines);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
